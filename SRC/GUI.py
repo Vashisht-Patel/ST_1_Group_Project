b00c0b6ef7e956c5215bc270c87f2512970bc445
Vashisht-Patel <u3330114@uni.canberra.edu.au>
2026-05-15 22:24:10 +1000

    Add header comments and metadata to GUI.py

--- SRC/GUI.py
+++ SRC/GUI.py
@@ -1,3 +1,19 @@
+""" 
+******************************* 
+Software Technology 1 
+Assessment 3 – Part A 
+Group: X 
+Authors: 
+u3330114 
+u3258243 
+u3298551 
+Date: 
+15/05/2026 
+******************************* 
+"""
+
+
+
 import tkinter as tk
 from tkinter import ttk
 from pathlib import Path
