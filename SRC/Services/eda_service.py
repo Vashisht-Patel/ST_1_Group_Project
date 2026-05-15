9101a36d34bc7d6902e73d0a1f7af87530a95d6f
Vashisht-Patel <u3330114@uni.canberra.edu.au>
2026-05-15 22:27:12 +1000

    Add header documentation to eda_service.py

--- SRC/Services/eda_service.py
+++ SRC/Services/eda_service.py
@@ -1,3 +1,18 @@
+""" 
+******************************* 
+Software Technology 1 
+Assessment 3 – Part A 
+Group: X 
+Authors: 
+u3330114 
+u3258243 
+u3298551 
+Date: 
+15/05/2026 
+******************************* 
+"""
+
+
 import config
 from pathlib import Path
 import matplotlib.pyplot as plt
